496117bc04a8ed400e6ab8081d100df050ece5a1
dzikoysk <dzikoysk@dzikoysk.net>
2020-09-15 03:11:22 +0200

    GH-2 Clear depth buffer bit and disable mobile distribution

--- gdx-vfx/core/src/com/crashinvaders/vfx/VfxManager.java
+++ gdx-vfx/core/src/com/crashinvaders/vfx/VfxManager.java
@@ -65,7 +65,7 @@ public final class VfxManager implements Disposable {
     private int width, height;
 
     public VfxManager(Format fboFormat) {
-        this(fboFormat, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), false);
+        this(fboFormat, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), true);
     }
 
     public VfxManager(Format fboFormat, int bufferWidth, int bufferHeight, boolean hasDepth) {
